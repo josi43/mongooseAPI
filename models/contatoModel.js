721a9d420e6805505020076479fcbd945a41e54a
josi43 <mateusseabra@hotmail.com>
2022-11-02 12:54:42 -0300

    atualizanod model

--- models/contatoModel.js
+++ models/contatoModel.js
@@ -5,14 +5,12 @@ const contatoSchema = new mongoose.Schema({
         type: String,
         required:[true,'Nome é obrigatório'],
         trim : true,
-        minLneght: [3,'nome desve conter pelo menos 3 caracteres']
+        minLength: [3,'nome desve conter pelo menos 3 caracteres']
     },
     fone: {
         type: String,
         required:[true,'fone é obrigatório']
     },
-},{
-    timestamps: true,
 });
 
 module.exports = mongoose.model('Contato', contatoSchema);
